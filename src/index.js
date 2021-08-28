02dda4aea50078445e86e9a358cf95b23d82310a
Ivan <ivan_grekov@yahoo.com>
2021-08-28 11:59:50 +0300

    feat: create the script to work with the stack

--- src/index.js
+++ src/index.js
@@ -11,5 +11,23 @@ module.exports = function check(str, bracketsConfig) {
     CLOSED_BRACKETS.push(bracketsConfig[i][1]);
   }
 
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    let indexOpenElem = OPEN_BRACKETS.indexOf(currentSymbol);
+    let indexClosedElem = CLOSED_BRACKETS.indexOf(currentSymbol);
+    let topElem = stack.length ? stack[stack.length - 1] : null;
+    if (indexOpenElem > -1 && topElem !== CLOSED_BRACKETS[indexOpenElem]) {
+      stack.push(currentSymbol);
+    } else {
+      if(stack.length === 0) {
+        return false
+      } else if (OPEN_BRACKETS[indexClosedElem] === topElem) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
   // your solution
 }
