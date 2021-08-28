833d2ca6ac9732c8375d15721ade714b27d09e4d
Ivan <ivan_grekov@yahoo.com>
2021-08-28 11:42:21 +0300

    feat: create the arrays open_brackets and closed_brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
+  let stack = [];
+  const OPEN_BRACKETS = [];
+  const CLOSED_BRACKETS = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    OPEN_BRACKETS.push(bracketsConfig[i][0]);
+  }
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    CLOSED_BRACKETS.push(bracketsConfig[i][1]);
+  }
+
   // your solution
 }
